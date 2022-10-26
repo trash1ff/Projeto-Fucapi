a6cb33746cf8f1709333c957e89fe17b911d2650
trash1ff <115058662+trash1ff@users.noreply.github.com>
2022-10-25 23:56:33 -0300

    Update EMPRESAFUCAPI.sql

--- EMPRESAFUCAPI.sql
+++ EMPRESAFUCAPI.sql
@@ -1,53 +1,22 @@
-CREATE DATABASE  IF NOT EXISTS `empresa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE  IF NOT EXISTS `empresa`;
 USE `empresa`;
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
---
--- Host: localhost    Database: empresa
--- ------------------------------------------------------
--- Server version	8.0.30
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `cargo`
---
 
 DROP TABLE IF EXISTS `cargo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `cargo` (
   `idcargo` int NOT NULL AUTO_INCREMENT,
   `nome_cargo` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`idcargo`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `cargo`
---
+);
 
 LOCK TABLES `cargo` WRITE;
-/*!40000 ALTER TABLE `cargo` DISABLE KEYS */;
+
 INSERT INTO `cargo` VALUES (1,'Vendedor'),(2,'Recepcionista'),(3,'Auxiliar administrativo'),(4,'Assistente administrativo'),(5,'Auxiliar Logístico');
-/*!40000 ALTER TABLE `cargo` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `funcionario`
---
-
 DROP TABLE IF EXISTS `funcionario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `funcionario` (
   `idfuncionario` int NOT NULL AUTO_INCREMENT,
   `senha` int NOT NULL,
@@ -58,26 +27,16 @@ CREATE TABLE `funcionario` (
   PRIMARY KEY (`idfuncionario`),
   KEY `id_cargo_idx` (`id_cargo`),
   CONSTRAINT `id_cargo` FOREIGN KEY (`id_cargo`) REFERENCES `cargo` (`idcargo`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `funcionario`
---
+) ;
 
 LOCK TABLES `funcionario` WRITE;
-/*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
+
 INSERT INTO `funcionario` VALUES (1,1234,'João Paulo',1234567890,1000,3),(2,5678,'Marcos Vinicius',1234567891,1100,2),(3,4321,'Maria Silva',1234567892,1200,1),(4,9876,'Jeferson Souza',1234567893,1300,4),(5,7689,'Paula Roberta',1234567894,1400,5);
-/*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
+
 UNLOCK TABLES;
 
---
--- Table structure for table `registro`
---
-
 DROP TABLE IF EXISTS `registro`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+
 CREATE TABLE `registro` (
   `idregistro` int NOT NULL AUTO_INCREMENT,
   `data_hora` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -85,26 +44,10 @@ CREATE TABLE `registro` (
   PRIMARY KEY (`idregistro`),
   KEY `id_funcionario_idx` (`id_funcionario`),
   CONSTRAINT `id_funcionario` FOREIGN KEY (`id_funcionario`) REFERENCES `funcionario` (`idfuncionario`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `registro`
---
+);
 
 LOCK TABLES `registro` WRITE;
-/*!40000 ALTER TABLE `registro` DISABLE KEYS */;
+
 INSERT INTO `registro` VALUES (1,'2022-09-09 05:49:58',5),(2,'2022-09-09 05:49:58',4),(3,'2022-09-09 05:49:58',3),(4,'2022-09-09 05:49:58',2),(5,'2022-09-09 05:49:58',1);
-/*!40000 ALTER TABLE `registro` ENABLE KEYS */;
+
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-09-12  1:26:43
